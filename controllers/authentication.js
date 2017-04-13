350436f2ce33a64b2c477783738385d8a55d461b
manjarb <manjarb4@gmail.com>
2017-04-13 18:30:35 +0700

    pp

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,4 +1,11 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config.js');
+
+function tokenForUser(user) {
+    const timeStamp = new Date().getTime();
+    return jwt.encode({ sub: user.id, iat: timeStamp }, config.secret);
+}
 
 exports.signup = function (req, res, next) {
     const email = req.body.email;
@@ -32,7 +39,7 @@ exports.signup = function (req, res, next) {
 
             // Respond to request indicating the user was created
             //res.json(user)
-            res.json({ success: true })
+            res.json({ token: tokenForUser(user) })
         });
 
     });
